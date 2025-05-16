27a3e5756d96b6a3ceaa6beb2cef88162c431d5b
Bhavani-U <ubhavanireddy65@gmail.com>
2025-05-16 21:55:41 +0530

    shell-script

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+echo "jaanu:: Hello Minnu, How are you?"
+echo "Minnu:: Hi Jaanu, I am fine. How are you?"
+echo "Jaanu:: I am fine too. how is your work?"
+echo "Minnu:: not bad. I am thinking to upgrade to DevOps"
