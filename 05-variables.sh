736338b2a89dff88a84b3f3108281a06db9abc72
Bhavani-U <ubhavanireddy65@gmail.com>
2025-05-16 21:59:56 +0530

    shell-script

---
+++ 05-variables.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+echo "Please enter username::"
+
+read USERNAME #here USERNAME is variable
+
+echo "Please enter password::"
+
+read PASSWORD
+
+echo "username is: $USERNAME, Passsword is: $PASSWORD"
